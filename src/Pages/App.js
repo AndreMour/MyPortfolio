0e2a652a814bfa618ccf65b764816fdcedc3ce11
AndreMour <andreluizmoura2004@outlook.com>
2024-03-03 17:24:02 -0300

    First Commit

---
+++ src/Pages/App.js
@@ -0,0 +1,16 @@
+import { BackContainer, Container, InnerContainer } from "./styles";
+
+
+function App() {
+  return (
+    <BackContainer>
+     <Container>
+      <InnerContainer>
+
+      </InnerContainer>
+     </Container>
+    </BackContainer>
+  );
+}
+
+export default App;
---
+++ src/Pages/styles.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const BackContainer = styled.div`
+    background-color: #24272C;
+`
+
+
+export const Container = styled.div`
+    min-height: 100vh;
+    width: 100%;
+    border:40px solid;
+    border-color: #24272C;
+    border-radius: 70px;
+    box-shadow: 10px 20px 10px 0 #00000040;
+
+`
+export const InnerContainer = styled.div`
+    display: flex;
+    min-height: 100vh;
+    width: 1035ps;
+    margin: 0;
+    padding: 1rem;
+    border-radius: 20px;
+    background-color: #232732;
+   
+
+`
